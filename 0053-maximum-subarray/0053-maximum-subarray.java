1a661a696df6a35459246c305472ffb1286affd5
Shlok Vyas <shlokvyas90@gmail.com>
2026-08-15 00:22:17 +0530

    Time: 1 ms (99.95%), Space: 77.4 MB (19.65%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
-        int currentSum = nums[0];
-        
-        for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(maxSum, currentSum);
+        int currentValue = nums[0];
+        int maxShlok = nums[0];
+        for(int i = 1;i<nums.length;i++){
+            currentValue = Math.max(nums[i],currentValue + nums[i]);
+            maxShlok = Math.max(maxShlok , currentValue);
         }
-        
-        return maxSum;
+        return maxShlok;
     }
 }
