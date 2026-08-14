702294bdbc2f162a5e8ccf438ca24ccbef9b7c18
Shlok Vyas <shlokvyas90@gmail.com>
2026-08-14 12:16:25 +0530

    Time: 2 ms (42.41%), Space: 94.6 MB (33.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-       int min = prices[0];
+        // buy is first day to buy stock because brfor shell stock buy stock 
+       int buy = prices[0];
        int max = 0;
        for(int i = 1;i<prices.length;i++){
-        if(prices[i]<min){
-            min = prices[i];
+        // compare to all array minimum value 
+        if(prices[i]<buy){
+            buy = prices[i];
         }
-        max = Math.max(max,prices[i]-min);
+        max = Math.max(max,prices[i]-buy);
 
        
     }return max;
