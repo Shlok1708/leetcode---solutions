a6fcd19091103b59c3217b440812c1a3fa815a51
Shlok Vyas <shlokvyas90@gmail.com>
2026-08-15 23:46:52 +0530

    Time: 1548 ms (12.29%), Space: 48.6 MB (70.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -6,7 +6,6 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int sum = 0;
             for (int j = i; j < n; j++) {
-                // पिछले सम में नया एलिमेंट जोड़ते जाएं
                 sum += nums[j];
                 
                 if (sum == k) {
