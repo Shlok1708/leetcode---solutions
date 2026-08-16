1f89c68dd707480632b52524e96e297463a354aa
Shlok Vyas <shlokvyas90@gmail.com>
2026-08-16 23:28:14 +0530

    Time: 3 ms (99.83%), Space: 46.5 MB (43.29%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String intToRoman(int num) {
+
+        int[] shlok = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] alok = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder sb = new StringBuilder();
+        
+
+        for (int i = 0; i < shlok.length && num > 0; i++) {
+            while (num >= shlok[i]) {
+                num -= shlok[i];
+                sb.append(alok[i]);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
