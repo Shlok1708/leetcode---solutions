a4501354ffaecc71bad54053d6dc8dc638b77660
Shlok Vyas <shlokvyas90@gmail.com>
2026-08-25 23:13:21 +0530

    Time: 0 ms (100%), Space: 43.3 MB (61.88%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        
-        for(int i = digits.length - 1; i >= 0; i--){
-            
-            if(digits[i] < 9){
+        for(int i = digits.length-1;i>=0;i--){
+            if(digits[i]<9){
                 digits[i]++;
                 return digits;
             }
-            
-            digits[i] = 0;
+            digits[i] =0;
         }
-        
-        int newArr[] = new int[digits.length + 1];
-        newArr[0] = 1;
-        
-        return newArr;
+        int shlok[] = new int [digits.length+1];
+        shlok[0] = 1;
+        return shlok;
     }
 }
