8311cb9d81229d172c1a1a8f768223d34e5d3726
Shlok Vyas <shlokvyas90@gmail.com>
2026-08-27 01:05:07 +0530

    Time: 8 ms (6.59%), Space: 46.9 MB (15.87%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -1,23 +1,13 @@
 class Solution {
     public int[] decompressRLElist(int[] nums) {
-      
-        int totalSize = 0;
-        for (int i = 0; i < nums.length; i += 2) {
-            totalSize += nums[i]; 
-        }
-        
-        int[] result = new int[totalSize];
-        int index = 0;
-        
-        for (int i = 0; i < nums.length; i += 2) {
-            int freq = nums[i];
-            int val = nums[i + 1];
-            
-            for (int j = 0; j < freq; j++) {
-                result[index++] = val;
+        List <Integer> result = new ArrayList<Integer>();
+        for(int i = 0;i<nums.length;i += 2){
+            int shlok = nums[i];
+            int alok =  nums[i+1];
+            for(int j = 0;j<shlok;j++){
+                result.add(alok);
             }
         }
-        
-        return result;
+         return result.stream().mapToInt(i -> i).toArray();
     }
 }
