5094b6deb266ad4dbe86f12342b15e9e73de6e72
Shlok Vyas <shlokvyas90@gmail.com>
2026-09-06 00:38:47 +0530

    Time: 5 ms (94.82%), Space: 46.3 MB (87.67%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String frequencySort(String s) {
+      
+        int[] count = new int[128];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
+        }
+        
+        StringBuilder sc = new StringBuilder();
+       
+        for (int i = 0; i < s.length(); ) {
+            int max = 0;
+            int maxChar = 0;
+         
+            for (int j = 0; j < 128; j++) {
+                if (count[j] > max) {
+                    max = count[j];
+                    maxChar = j;
+                }
+            }
+           
+            if (max == 0) break;
+            
+            for (int k = 0; k < max; k++) {
+                sc.append((char) maxChar);
+            }
+            
+            count[maxChar] = 0;
+            i += max;
+        }
+        
+        return sc.toString();
+    }
+}
