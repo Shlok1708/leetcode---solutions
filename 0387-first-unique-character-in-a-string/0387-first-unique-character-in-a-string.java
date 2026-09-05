ac358f808371b3ea7c8739b359aab78eb6e8fc76
Shlok Vyas <shlokvyas90@gmail.com>
2026-09-05 23:40:12 +0530

    Time: 8 ms (53.14%), Space: 47 MB (39.84%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,17 +1,16 @@
 class Solution {
     public int firstUniqChar(String s) {
-     
-        int[] count = new int[26];
-        
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
+
+        int [] shlok = new int[26];
+        for(int i = 0;i<s.length();i++){
+            shlok[s.charAt(i) - 'a']++;
         }
-      
-        for (int i = 0; i < s.length(); i++) {
-            if (count[s.charAt(i) - 'a'] ==1) {
-                return i; 
+         for(int i = 0;i<s.length();i++){
+            if(shlok[s.charAt(i)-'a'] == 1){
+                return i;
             }
-        }
+         }
         return -1;
+
     }
 }
