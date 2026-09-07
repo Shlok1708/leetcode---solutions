fddd11331e14ac833530e1d97d2e08c8f94b4f61
Shlok Vyas <shlokvyas90@gmail.com>
2026-09-08 00:36:48 +0530

    Time: 0 ms (100%), Space: 43.1 MB (82.6%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,14 +1,17 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
         
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) return "";
+        if(strs.length == 0|| strs == null){
+         return "";
+        }
+        String shlok = strs[0];
+        for(int i = 1;i<strs.length;i++){
+            while(strs[i].indexOf(shlok) != 0){
+                shlok = shlok.substring(0,shlok.length()-1);
+                if(shlok.isEmpty()) return "";
             }
         }
-        return prefix;
+        return shlok;
     }
+
 }
