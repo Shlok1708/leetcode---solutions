1606250b66e6193373179ff972e2cf3d78da48d3
Shlok Vyas <shlokvyas90@gmail.com>
2026-09-18 23:40:59 +0530

    Time: 24 ms (28.37%), Space: 92.7 MB (80.1%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> shlok = new HashSet<>();
-        for(int i = 0;i<nums.length;i++){
-            shlok.add(nums[i]);
+        for(int i : nums){
+            shlok.add(i);
         }
         return nums.length > shlok.size(); 
     }
